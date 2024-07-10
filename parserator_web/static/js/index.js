e8403f911e4f0b4a8e534816bfd18b06d28bfb9b
Jack Gibson <jpgibson@uchicago.edu>
2024-07-09 19:31:12 -0500

    working version

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,59 +1,61 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
 
-const url = "http://localhost:8000/api/parse/";
+   const url = "http://localhost:8000/api/parse/";
 
-async function makeGet(event){
-   event.preventDefault();
+   // source for reseting HTML table: https://stackoverflow.com/questions/47214759/how-to-reset-an-html-table-to-its-original-after-changing-it-with-a-javascript-f
+   const blankTable = document.getElementById("results_table").innerHTML
 
-   let input_string = inputForm.elements['address'].value;
+   document.getElementById('input_form').onsubmit = async function(event) {
+       event.preventDefault();
+   
+       let input_string = document.getElementById('input_form').elements['address'].value;
+       console.log(input_string); 
+   
+       const queryTerms = new URLSearchParams({ address: input_string });
+   
+       try {
+           const response = await fetch(`${url}?${queryTerms}`, {
+               method: 'GET'
+           });
 
-   const queryTerms = new URLSearchParams({address : input_string});
+           const respData = await response.json()
+   
+           if (!response.ok) {
+               alert(respData.detail);
+           }
+           
+   
+           let results = document.getElementById("address-results");
+           results.style.display = "block";
+   
+           document.getElementById("parse-type").textContent = respData.address_type;
+   
+           let addressParts = respData.address_components;
 
-   try {
-      const response = await fetch(`${url}?${queryTerms}`, {
-         method: 'GET'
-      });
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-      const json = await response.json();
+           let table = document.getElementById("results_table");
+           table.innerHTML = blankTable; 
+   
 
-      let results = document.getElementById("address-results")
+           for (let tag in addressParts) {
+               if (addressParts.hasOwnProperty(tag)) {
+                   let part = addressParts[tag];
+                   console.log(tag, part);
+   
+                   let nextRow = table.insertRow(-1);
+                   let partCell = nextRow.insertCell(0);
+                   let resultsCell = nextRow.insertCell(1);
+   
+                   partCell.appendChild(document.createTextNode(tag));
+                   resultsCell.appendChild(document.createTextNode(part));
+               }
+           }
+        
+       } catch (error) {
+           console.error(error.message);
+       }
+   };
 
-      results.style.display = "block";
-
-      document.getElementById("parse-type").textContent += json.address_type
-
-      let addressParts = json.address_components
-
-      let table = document.getElementById("results_table")
-
-      for (let tag in addressParts) {
-         if (addressParts.hasOwnProperty(tag)) {
-
-            let part = addressParts[tag]
-            console.log(tag, part)
-
-            let nextRow = table.insertRow(-1);
-
-            let partCell = nextRow.insertCell(0);
-            let resultsCell = nextRow.insertCell(1);
-
-            partCell.appendChild(document.createTextNode(tag));
-            resultsCell.appendChild(document.createTextNode(part));
-
-         }
-      }
-
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
-
-const inputForm = document.getElementById("input_form");
-inputForm.addEventListener("submit", makeGet);
-
+   
 
 
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+from usaddress import RepeatedLabelError
 from rest_framework.exceptions import ParseError
 
 
@@ -14,20 +15,24 @@ class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        params = {}
+        params = {"input_string": None, 
+                    "address_components" : None,
+                    "address_type" : None} 
+
         input_string = request.GET.get("address")
-        print(input_string)
-        if input_string:
+       
+        if not input_string:
+            return Response(params) 
+
+        try:
             address_comps, address_type = self.parse(input_string)
-            params = {"input_string": input_string, 
-                      "address_components" : address_comps,
-                      "address_type" : address_type}
+        except RepeatedLabelError as e:
+            raise ParseError(f"Error parsing inputed string: {e.original_string}. Address may have duplicated parts")
         
-        print(params)
-        return Response(params) 
+        return Response({"input_string": input_string, 
+                    "address_components" : address_comps,
+                    "address_type" : address_type}) 
 
     def parse(self, address):
         address_components, address_type = usaddress.tag(address)
-        # TODO: Implement this method to return the parsed components of a
-        # given address using usaddress: https://github.com/datamade/usaddress
         return address_components, address_type
